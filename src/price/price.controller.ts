02212daeab1b3c35b04ad7f93087ae3272c5fe4d
Dineshkr23 <dksharma25@live.com>
2026-03-24 12:13:28 +0530

    Changes: GET Price API Made Open

--- src/price/price.controller.ts
+++ src/price/price.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Public } from '../auth/decorators/public.decorator';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UpdateGlobalPriceDto } from './dto/update-global-price.dto';
 import { PriceService } from './price.service';
@@ -8,7 +9,7 @@ export class PriceController {
   constructor(private readonly priceService: PriceService) {}
 
   @Get()
-  @Roles('ADMIN', 'PRICE_EDITOR')
+  @Public()
   async getPrice() {
     return this.priceService.getGlobalPrice();
   }
